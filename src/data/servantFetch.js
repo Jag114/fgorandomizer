4b8b33ab02bd89d6c740eec467d9e55986f82742
Jag114 <49473034+Jag114@users.noreply.github.com>
2022-09-21 17:42:58 +0200

    fetch fix, image caching (currently disabled)

--- src/data/servantFetch.js
+++ src/data/servantFetch.js
@@ -1,16 +1,27 @@
 /* creates empty array for filtered data, 
 * fetches array of objects and filters them based on settings (object) argument,
 * filtered objects are put into modifiedData array which is later returned
+* deletes ids of unplayable characters
 */
+//import cacheData from './cacheData';
 
 async function fetchServant(settings) {
     //console.log("Settings: ",settings);
     let modifiedData = [];
     let isTrue;
+    let unplayableID = [83, 149, 151, 168, 152, 240] //151 and 152 cant be one after the other, splice changes array length
     //const response = await fetch('servants.json')  // .servants
     const response = await fetch('https://api.atlasacademy.io/export/NA/basic_servant.json')
-    const data = await response.json()
-    //console.log(data);
+    let data = await response.json()
+    for(let i = 0; i < data.length; i++){
+        for (let j = 0; j < unplayableID.length; j++) {
+            if(data[i].collectionNo === unplayableID[j]){
+                data.splice(i,1);
+                unplayableID.splice(j,1)
+            }
+        }
+    }
+
     if(settings.rarity.length > 0){
         data.forEach(e => {
             settings.rarity.forEach(raritySetting => {
@@ -33,7 +44,8 @@ async function fetchServant(settings) {
             return isTrue ? true : false
        })
     }
-    //console.log(modifiedData);
+    console.log(modifiedData);
+    //cacheData(data);  not sure if works/worth it
     return { data: modifiedData, length: modifiedData.length };
 }
 
