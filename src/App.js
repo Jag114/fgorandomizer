77be3f74228a4803fb37fcda8104b5e334b53cb2
Jag114 <49473034+Jag114@users.noreply.github.com>
2022-10-26 19:03:14 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Party from './components/Party';
 import SettingsMenu from './components/SettingsMenu';
 import useSettings from './hooks/useSettings';
 import UserServantList from './components/UserServantList';
+import InfoFooter from "./components/InfoFooter";
 
 const App = () => {
 
   const [formData, setFormData, region, setRegion ] = useSettings()
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={
           <>
             <Party formData={formData} region={region}/>
             <SettingsMenu formData={formData} setFormData={setFormData} region={region} setRegion={setRegion}/>
+            <InfoFooter/>
           </>
         }/>
         <Route path="profile" element={<UserServantList formData={formData} region={region}/>}/>
