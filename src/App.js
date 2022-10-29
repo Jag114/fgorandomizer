c139644ddc934d3d4a553ccfa89a8e64f0c9817f
Jag114 <49473034+Jag114@users.noreply.github.com>
2022-10-29 18:40:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Party from './components/Party';
 import SettingsMenu from './components/SettingsMenu';
 import useSettings from './hooks/useSettings';
-import UserServantList from './components/UserServantList';
 import InfoFooter from "./components/InfoFooter";
 
 const App = () => {
@@ -10,18 +8,11 @@ const App = () => {
   const [formData, setFormData, region, setRegion ] = useSettings()
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Party formData={formData} region={region}/>
-            <SettingsMenu formData={formData} setFormData={setFormData} region={region} setRegion={setRegion}/>
-            <InfoFooter/>
-          </>
-        }/>
-        <Route path="profile" element={<UserServantList formData={formData} region={region}/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+    <Party formData={formData} region={region}/>
+    <SettingsMenu formData={formData} setFormData={setFormData} region={region} setRegion={setRegion}/>
+    <InfoFooter/>
+  </>
   );
 }
 
